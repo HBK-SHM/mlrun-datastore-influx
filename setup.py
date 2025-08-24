3f02638d30da6b94a0a2ab6937ad919bb2ad729e
graman-hbk <92294263+graman-hbk@users.noreply.github.com>
2025-08-24 07:09:45 +0530

    Baseline influx plugin for mlrun datastores

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlrun-influx-plugin",
-    version="0.1.0",
+    version="0.0.1",
     packages=find_packages(),
-    install_requires=["mlrun>=1.6.0", "influxdb-client>=1.39"],
+    install_requires=["mlrun>=1.9.2", "influxdb-client>=1.39"],
     entry_points={
         "mlrun.datastore": [
             "influx = mlrun_influx_store:InfluxStore"
